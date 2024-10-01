1688bffed224a55da253bb3d52fd0eeb50f2dd25
amaben2020 <amaben202phantom@gmail.com>
2024-10-01 23:18:03 +0100

    extracting logic to core

--- packages/functions/src/jobs/list.ts
+++ packages/functions/src/jobs/list.ts
@@ -1,7 +1,5 @@
 import { list } from '@/core/jobs';
 import { Util } from '@/core/utils/TupApihandler';
-import { useDb } from '@/functions/db';
-import { jobs as jobsSchema } from '@/functions/db/schema/job';
 
 export const main = Util.handler(async (event) => {
   const jobs = await list();
