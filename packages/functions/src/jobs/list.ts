fb1fdb48d68778e4f1d3affaecee4e667caddc0c
amaben2020 <amaben202phantom@gmail.com>
2024-10-01 23:16:01 +0100

    updating tables with jobs and jobRequest

--- packages/functions/src/jobs/list.ts
+++ packages/functions/src/jobs/list.ts
@@ -3,6 +3,7 @@ import { useDb } from '@/functions/db';
 import { jobs as jobsSchema } from '@/functions/db/schema/job';
 
 export const main = Util.handler(async (event) => {
+  //TODO: Extract to jobs core for listing jobs
   const jobs = await useDb().select().from(jobsSchema).execute();
 
   return {
