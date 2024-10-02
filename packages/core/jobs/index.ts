57e7bde54759a31106dac74710114fa6e14c7015
amaben2020 <amaben202phantom@gmail.com>
2024-10-02 16:54:56 +0100

    addding

--- packages/core/jobs/index.ts
+++ packages/core/jobs/index.ts
@@ -3,6 +3,6 @@ import { jobs } from '@/functions/db/schema/job';
 
 export const list = async () => {
   const res = await useDb().select().from(jobs).execute();
-
+  console.log('object' == 'object');
   return res;
 };
